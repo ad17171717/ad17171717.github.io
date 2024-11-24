5f5b2dba2a6b00cc6c35e6f467f431f4e3d812ad
Adrian D <ad17171717@gmail.com>
2024-11-24 09:02:59 -0700

    Update podcast page and script.js to load YT eps

--- script.js
+++ script.js
@@ -8,26 +8,4 @@ document.addEventListener('DOMContentLoaded', () => {
     burger.addEventListener('click', () => {
         navLinks.classList.toggle('nav-active');
     });
-
-    const episodeSelect = document.getElementById('episode-select');
-    const podcastVideo = document.getElementById('podcast-video');
-
-    const episodes = {
-        episode1: "https://www.youtube.com/embed/Rbjf5pnkK4w",
-        episode2: "https://www.youtube.com/embed/TIpb_2nspk0",
-        episode3: "https://www.youtube.com/embed/HDTf9yqTUzI",
-        episode4: "https://www.youtube.com/embed/VP1uZ51vHm4",
-        episode5: "https://www.youtube.com/embed/0vL5qU5qWJg"
-    };
-
-    episodeSelect.addEventListener('change', function () {
-        const selectedEpisode = this.value;
-        if (episodes[selectedEpisode]) {
-            console.log(`Selected Episode: ${selectedEpisode}`);
-            console.log(`Updating iframe src to: ${episodes[selectedEpisode]}`);
-            podcastVideo.src = `${episodes[selectedEpisode]}&autoplay=1`;
-        } else {
-            console.error("Selected episode not found in episodes map.");
-        }
-    });
 });
